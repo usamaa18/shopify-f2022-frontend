f08fdc078843756484b1c01f726cb18f2fbf8f62
Usama Ali <usamaa18@gmail.com>
2022-05-17 16:11:21 -0600

    replaced tailwind from cdn to npm

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        color1: '#e2ded0',
+        color2: '#000040'
+        
+      },
+      screens: {
+        'sm': '640px',
+        'md': '640px',
+        'lg': '640px',
+        'xl': '640px',
+        '2xl': '640px',
+      }
+    }
+  },
+  plugins: []
+}
